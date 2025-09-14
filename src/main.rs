2ba159181a062008a6a550faac7bf38a5edd79a4
ruben <ruben@me.com>
2025-09-14 20:18:56 +0200

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 /// A "Hello, world!" program.
 
 fn main() {
+    hello_world();
+    goodbye_world();
+}
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+fn goodbye_world() {
+    println!("Goodbye, world!");
 }
